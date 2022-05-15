cc33653460ff484297363737a0051dfd073829e8
anhtuanpham12101996 <anhtuanpham12101996@outlook.com>
2022-05-15 16:20:11 +0700

    3 files modified, 2 added

---
+++ src/app/Task_ReviewCourse/review-course/review-course.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-review-course',
+  templateUrl: './review-course.component.html',
+  styleUrls: ['./review-course.component.css']
+})
+export class ReviewCourseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { ReviewCourseComponent } from './Task_ReviewCourse/review-course/review-course.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent, HelloComponent, ReviewCourseComponent],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
